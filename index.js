8a3553e3ef4968ccc291a8660ab0d00637bf81b5
Nick Foden <nickfoden@gmail.com>
2017-11-12 13:45:54 -0500

    Adding simple menu with quit option. Not wired yet.

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const electron = require('electron');
 
-const {app, BrowserWindow } = electron;
+const {app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/main.html`)
-})
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+//Each object in menuTemplate refers to each menu dropdown - file edit etc
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Quit'
+            }
+        ]
+    }
+];
